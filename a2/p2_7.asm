a63a028fb6cdc77f4710c676d2d68f088e09a43b
Anubhab Rakshit <anubhabrakshit.06@gmail.com>
2026-09-07 16:04:12 +0530

    p2_7.asm fixed

--- a2/p2_7.asm
+++ a2/p2_7.asm
@@ -1,118 +1,161 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-    msg1 DB 'Original string: hello world', 13, 10, '$'
-    msg2 DB 'After deleting "hello ": $'
-    str1 DB 'hello world$'
+    msg1 DB 'Enter main string: $'
+    msg2 DB 13, 10, 'Enter sub-string to delete: $'
+    msg3 DB 13, 10, 'Resulting string: $'
+    msg4 DB 13, 10, 'Sub-string not found.$'
+    
+    str_buf DB 50
+    str_len DB ?
+    str_data DB 50 DUP('$')
+    
+    sub_buf DB 50
+    sub_len DB ?
+    sub_data DB 50 DUP('$')
+    
 .CODE
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
-
+    MOV ES, AX
+    
+    ; Print msg1
     LEA DX, msg1
     MOV AH, 09H
     INT 21H
-
+    
+    ; Read main string
+    LEA DX, str_buf
+    MOV AH, 0AH
+    INT 21H
+    
+    ; Print msg2
     LEA DX, msg2
     MOV AH, 09H
     INT 21H
-
-    MOV SI, OFFSET str1
-    ADD SI, 6 ; skip 'hello '
-PRINT_STR:
-    MOV DL, [SI]
-    CMP DL, '$'
-    JE DONE
-    MOV AH, 02H
+    
+    ; Read sub-string
+    LEA DX, sub_buf
+    MOV AH, 0AH
     INT 21H
-    INC SI
-    JMP PRINT_STR
-DONE:
-    CALL NEWLINE
-
-    MOV AH, 4CH
-    INT 21H
-MAIN ENDP
-
-; Utility Procedures
-GET_NUM PROC
-    PUSH BX
-    PUSH CX
-    PUSH DX
-    MOV BX, 0
-    MOV CX, 10
-    MOV AH, 01H
-READ_CHAR:
-    INT 21H
-    CMP AL, 13
-    JE END_GET_NUM
-    CMP AL, 32
-    JE END_GET_NUM
-    SUB AL, '0'
+    
+    ; Check if sub_len is 0
+    CMP sub_len, 0
+    JE PRINT_RESULT
+    
+    ; Check if str_len is 0
+    MOV CL, str_len
+    MOV CH, 0
+    CMP CX, 0
+    JE PRINT_RESULT
+    
+    MOV AL, sub_len
     MOV AH, 0
-    PUSH AX
-    MOV AX, BX
-    MUL CX
-    POP DX
+    MOV DX, AX ; DX = sub_len (using DX instead of BX because BX is used for addressing)
+    
+    MOV SI, 0 ; i = 0
+SEARCH_LOOP:
+    ; if i + sub_len > str_len, not found
+    MOV AX, SI
     ADD AX, DX
-    MOV BX, AX
-    MOV AH, 01H
-    JMP READ_CHAR
-END_GET_NUM:
-    MOV AX, BX
-    POP DX
-    POP CX
-    POP BX
-    RET
-GET_NUM ENDP
-
-PRINT_NUM PROC
-    PUSH AX
-    PUSH BX
-    PUSH CX
-    PUSH DX
-    CMP AX, 0
-    JGE POSITIVE
-    PUSH AX
-    MOV DL, '-'
-    MOV AH, 02H
-    INT 21H
-    POP AX
-    NEG AX
-POSITIVE:
+    MOV BX, 0
+    MOV BL, str_len
+    CMP AX, BX
+    JG NOT_FOUND
+    
+    ; Match inner loop
+    MOV DI, 0 ; j = 0
+MATCH_LOOP:
+    CMP DI, DX
+    JAE MATCH_FOUND ; matched entirely!
+    
+    ; Compare str_data[i+j] with sub_data[j]
+    LEA BX, str_data
+    ADD BX, SI
+    ADD BX, DI
+    MOV AL, [BX]
+    
+    LEA BX, sub_data
+    ADD BX, DI
+    MOV AH, [BX]
+    
+    CMP AL, AH
+    JNE NO_MATCH
+    
+    INC DI
+    JMP MATCH_LOOP
+    
+NO_MATCH:
+    INC SI
+    JMP SEARCH_LOOP
+    
+MATCH_FOUND:
+    ; Shift left from i + sub_len to str_len
+    MOV AX, SI
+    ADD AX, DX
+    MOV DI, AX ; DI = i + sub_len (source index)
+    MOV BP, SI ; BP = i (dest index)
+    
     MOV CX, 0
-    MOV BX, 10
-DIV_LOOP:
-    MOV DX, 0
-    DIV BX
-    PUSH DX
-    INC CX
-    CMP AX, 0
-    JNE DIV_LOOP
-PRINT_LOOP:
-    POP DX
-    ADD DL, '0'
-    MOV AH, 02H
+    MOV CL, str_len
+    SUB CX, DI ; CX = bytes to move
+    
+    CMP CX, 0
+    JE UPDATE_LEN
+    
+SHIFT_LOOP:
+    LEA BX, str_data
+    ADD BX, DI
+    MOV AL, [BX]
+    
+    LEA BX, str_data
+    ADD BX, BP
+    MOV [BX], AL
+    
+    INC DI
+    INC BP
+    LOOP SHIFT_LOOP
+    
+UPDATE_LEN:
+    ; str_len -= sub_len
+    MOV AL, str_len
+    SUB AL, sub_len
+    MOV str_len, AL
+    JMP PRINT_RESULT
+    
+NOT_FOUND:
+    LEA DX, msg4
+    MOV AH, 09H
     INT 21H
-    LOOP PRINT_LOOP
-    POP DX
-    POP CX
-    POP BX
-    POP AX
-    RET
-PRINT_NUM ENDP
-
-NEWLINE PROC
-    PUSH AX
-    PUSH DX
+    JMP EXIT_PROG
+    
+PRINT_RESULT:
+    LEA DX, msg3
+    MOV AH, 09H
+    INT 21H
+    
+    ; Add '$' at the new end of str_data
+    MOV AL, str_len
+    MOV AH, 0
+    LEA BX, str_data
+    ADD BX, AX
+    MOV BYTE PTR [BX], '$'
+    
+    LEA DX, str_data
+    MOV AH, 09H
+    INT 21H
+    
+    ; Print newline
     MOV DL, 13
     MOV AH, 02H
     INT 21H
     MOV DL, 10
     MOV AH, 02H
     INT 21H
-    POP DX
-    POP AX
-    RET
-NEWLINE ENDP
+    
+EXIT_PROG:
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
 END MAIN
