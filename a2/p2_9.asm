1129f222b2d8eeca9569b6946a2ab0e84e26834c
Anubhab Rakshit <anubhabrakshit.06@gmail.com>
2026-09-14 14:55:22 +0530

    p2_9.asm fixed

--- a2/p2_9.asm
+++ a2/p2_9.asm
@@ -1,42 +1,112 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-    arr DW 10, 20, 30, 40, 50
-    msg1 DB 'Array: 10, 20, 30, 40, 50', 13, 10, '$'
-    msg2 DB 'Enter number to search (Linear): $'
-    msg3 DB 'Found!', 13, 10, '$'
-    msg4 DB 'Not Found!', 13, 10, '$'
+    arr DW 5 DUP(?)
+    msg_input DB 'Enter 5 numbers in SORTED order:', 13, 10, '$'
+    msg_elem DB 'Element: $'
+    msg_search_lin DB 'Enter number to search (Linear Search): $'
+    msg_search_bin DB 'Enter number to search (Binary Search): $'
+    msg_found DB 'Found!', 13, 10, '$'
+    msg_not_found DB 'Not Found!', 13, 10, '$'
+    search_key DW ?
 .CODE
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
 
-    LEA DX, msg1
+    LEA DX, msg_input
     MOV AH, 09H
     INT 21H
 
-    LEA DX, msg2
+    MOV CX, 5
+    MOV SI, OFFSET arr
+READ_ARRAY:
+    LEA DX, msg_elem
     MOV AH, 09H
     INT 21H
     CALL GET_NUM
+    MOV [SI], AX
+    CALL NEWLINE
+    ADD SI, 2
+    LOOP READ_ARRAY
+
+    ; Linear Search
+    LEA DX, msg_search_lin
+    MOV AH, 09H
+    INT 21H
+    CALL GET_NUM
+    MOV search_key, AX
     CALL NEWLINE
 
     MOV CX, 5
     MOV SI, OFFSET arr
-SEARCH:
+    MOV AX, search_key
+LIN_SEARCH:
     CMP AX, [SI]
-    JE FOUND
+    JE LIN_FOUND
     ADD SI, 2
-    LOOP SEARCH
+    LOOP LIN_SEARCH
 
-    LEA DX, msg4
+    LEA DX, msg_not_found
+    MOV AH, 09H
+    INT 21H
+    JMP BINARY_SEARCH_START
+LIN_FOUND:
+    LEA DX, msg_found
+    MOV AH, 09H
+    INT 21H
+
+BINARY_SEARCH_START:
+    ; Binary Search
+    LEA DX, msg_search_bin
+    MOV AH, 09H
+    INT 21H
+    CALL GET_NUM
+    MOV search_key, AX
+    CALL NEWLINE
+
+    MOV BX, 0    ; low index
+    MOV DX, 4    ; high index
+BIN_SEARCH_LOOP:
+    CMP BX, DX
+    JG BIN_NOT_FOUND
+    
+    ; mid = (low + high) / 2
+    MOV AX, BX
+    ADD AX, DX
+    SHR AX, 1    ; AX = mid index
+    
+    MOV SI, AX
+    SHL SI, 1    ; word offset
+    ADD SI, OFFSET arr
+    MOV CX, [SI] ; CX = arr[mid]
+    
+    MOV AX, search_key
+    CMP AX, CX
+    JE BIN_FOUND
+    JL SEARCH_LEFT
+    
+    ; SEARCH_RIGHT: low = mid + 1
+    MOV BX, AX
+    INC BX
+    JMP BIN_SEARCH_LOOP
+    
+SEARCH_LEFT:
+    ; high = mid - 1
+    MOV DX, AX
+    DEC DX
+    JMP BIN_SEARCH_LOOP
+    
+BIN_NOT_FOUND:
+    LEA DX, msg_not_found
     MOV AH, 09H
     INT 21H
     JMP DONE
-FOUND:
-    LEA DX, msg3
+BIN_FOUND:
+    LEA DX, msg_found
     MOV AH, 09H
     INT 21H
+
 DONE:
     MOV AH, 4CH
     INT 21H
@@ -49,13 +119,33 @@ GET_NUM PROC
     PUSH DX
     MOV BX, 0
     MOV CX, 10
+    
+SKIP_NON_DIGIT:
     MOV AH, 01H
+    INT 21H
+    CMP AL, 13
+    JE END_GET_NUM
+    CMP AL, '0'
+    JB SKIP_NON_DIGIT
+    CMP AL, '9'
+    JA SKIP_NON_DIGIT
+    
+    SUB AL, '0'
+    MOV AH, 0
+    MOV BX, AX
+
 READ_CHAR:
+    MOV AH, 01H
     INT 21H
     CMP AL, 13
     JE END_GET_NUM
     CMP AL, 32
     JE END_GET_NUM
+    CMP AL, '0'
+    JB READ_CHAR
+    CMP AL, '9'
+    JA READ_CHAR
+    
     SUB AL, '0'
     MOV AH, 0
     PUSH AX
@@ -64,7 +154,6 @@ READ_CHAR:
     POP DX
     ADD AX, DX
     MOV BX, AX
-    MOV AH, 01H
     JMP READ_CHAR
 END_GET_NUM:
     MOV AX, BX
@@ -73,42 +162,6 @@ END_GET_NUM:
     POP BX
     RET
 GET_NUM ENDP
-
-PRINT_NUM PROC
-    PUSH AX
-    PUSH BX
-    PUSH CX
-    PUSH DX
-    CMP AX, 0
-    JGE POSITIVE
-    PUSH AX
-    MOV DL, '-'
-    MOV AH, 02H
-    INT 21H
-    POP AX
-    NEG AX
-POSITIVE:
-    MOV CX, 0
-    MOV BX, 10
-DIV_LOOP:
-    MOV DX, 0
-    DIV BX
-    PUSH DX
-    INC CX
-    CMP AX, 0
-    JNE DIV_LOOP
-PRINT_LOOP:
-    POP DX
-    ADD DL, '0'
-    MOV AH, 02H
-    INT 21H
-    LOOP PRINT_LOOP
-    POP DX
-    POP CX
-    POP BX
-    POP AX
-    RET
-PRINT_NUM ENDP
 
 NEWLINE PROC
     PUSH AX
